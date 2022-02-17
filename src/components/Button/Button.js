f19ee4bba0b39ad16e0771c55fd59283a9b8b52c
rnferrer <rnferrer@ucdavis.edu>
2022-02-16 23:33:49 -0800

    added styling for buttons and navbar

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -12,7 +12,7 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
   
   return(
     <Link to='events' className="btn-mobile">
-      <button>
+      <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
         {children}
       </button>
     </Link>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,27 @@ import react, {useState} from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
+import { Button } from "../Button/Button";
 import logo from '../../mkBlack.png';
 import './Navbar.css'
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-
   const closeMobileMenu = () => setClick(false);
+  
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return(
     <>
 
