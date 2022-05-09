686082c2d57c5bdfdb92008053d0e490547f6722
rnferrer <50218266+rnferrer@users.noreply.github.com>
2022-05-09 11:38:24 -0700

    Fix winter events

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -4,43 +4,6 @@ import EventCard from '../EventCard/EventCard'
 import {BASE_URL} from '../../config' 
 
 function Events () {
-  const info = [
-    {
-      "title": "MK General Meeting",
-      "time": "7-9pm",
-      "date": "Oct 2",
-      "day": "Wed",
-      "place": "123 Science Lec Hall",
-    },
-    {
-      "title": "BSLS Reveal",
-      "time": "8pm",
-      "date": "Oct 27",
-      "day": "Friday",
-      "place": "Hutchinson Parking Structure",
-    },
-    {
-      "title": "PCN Symposium",
-      "time": "8pm",
-      "date": "Nov 7",
-      "day": "Friday",
-      "place": "Wellman 1",
-    },
-    {
-      "title": "Friendship Games",
-      "time": "8pm",
-      "date": "Oct 15-Oct 16",
-      "day": "Sat-Sun",
-      "place": "CSU Fullerton",
-    },
-    {
-      "title": "FATDs",
-      "time": "8pm",
-      "date": "Sep 30",
-      "day": "Friday",
-      "place": "Davis Elementary School",
-    }
-  ]
 
   const [fallEvents, setFallEvents] = useState([])
   const [winterEvents, setWinterEvents] = useState([])
@@ -57,7 +20,7 @@ function Events () {
         if (event.quarter==='Fall'){
           setFallEvents([...fallEvents, event])
         }else if (event.quarter==='Winter'){
-          setWinterEvents(...[winterEvents, event])
+          setWinterEvents([...winterEvents, event])
         }else if(event.quarter==='Spring'){
           setSpringEvents([...springEvents, event])
         }
